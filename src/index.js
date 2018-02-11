a755cf6d05e23c2db80259adfa673b3edccfa670
Hitesh297 <hites_297@yahoo.co.in>
2018-02-11 17:26:11 +0530

    login page
    ->url to be replaced with actual API
    ->API should return true for authorized users and false for unauthorized

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Routes from './routes';
+import {createStore, applyMiddleware} from 'redux';
+import ReduxPromise from 'redux-promise';
+import reducers from './reducers';
+import {Provider} from 'react-redux';
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDom.render(
+	 <Provider store={createStoreWithMiddleware(reducers)}>
 	<Routes />
+	  </Provider>
 	,document.querySelector('.container')
 	);
