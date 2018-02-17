d6c6aa296a01a367c7471b9fc39eb36614f24cae
Hitesh297 <hites_297@yahoo.co.in>
2018-02-18 00:42:43 +0530

    list added

--- src/components/app.js
+++ src/components/app.js
@@ -31,34 +31,36 @@ export default class App extends Component {
         <div className="d-flex align-items-center p-3 my-3 text-white-50 bg-grey rounded box-shadow">
         <img className="mr-3" src="https://cdn2.iconfinder.com/data/icons/images-and-video/36/27_camera_photo_video_capture_device_focus_flash_photography-512.png" alt="" width="48" height="48"/>
         <div className="lh-100">
-          <h5 className="mb-0 text-white lh-100">Welcome ! Speak you mind out, let's make this world a better place to live.</h5>  
+          <h5 className="mb-0 text-white lh-100">Speak your mind out, let's make this world a better place to live.</h5>  
         </div>
       	</div>
 
 
       	  <div className="my-3 p-3 bg-white rounded box-shadow">
-        <h6 className="border-bottom border-gray pb-2 mb-0">Recent updates</h6>
-
-		<div className="media text-muted pt-3">
-          <ul>
-          <li>
-          <p className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-            <strong className="d-block text-gray-dark">@username</strong>
-            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-          </p>
-          </li>
-          <li>
-          <p className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-            <strong className="d-block text-gray-dark">@username</strong>
-            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-          </p>
-          </li>
-          </ul>
+        <h6 className="border-gray pb-2 mb-0">Recent updates</h6>
         </div>
 
 
+        <ul className="list-unstyled">
+        <li>
+        <div className="my-3 p-3 bg-white rounded box-shadow">
 
+          <p className="media-body pb-3 mb-0 small lh-125  border-gray">
+            <strong className="d-block text-gray-dark">@username</strong>
+            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+          </p>
+         </div>
+         </li>
+         <li>
+         <div className="my-3 p-3 bg-white rounded box-shadow">
+          <p className="media-body pb-3 mb-0 small lh-125 border-gray">
+            <strong className="d-block text-gray-dark">@username</strong>
+            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+          </p>
         </div>
+        </li>
+        </ul>
+
         </main>
 				
 			</div>
